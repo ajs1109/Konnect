04733bea435af3ee7ce152d8691a3f96ad3993b1
ajs1109 <78317286+ajs1109@users.noreply.github.com>
2023-02-06 02:52:32 +0530

    Creating forms for sign up and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,19 @@
 module.exports.profile = function(req,res) {
     return res.render('user_profile',{title: "User Profile"});
 }
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{title:"Konnect Sign Up"})
+}
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{title:"Konnect Sign In"})
+}
+
+module.exports.create = function(req,res){
+
+}
+
+module.exports.createSession = function(req,res){
+    
+}
