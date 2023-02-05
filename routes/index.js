789e20b1712161358c0ef72896c681be89963f52
ajs1109 <78317286+ajs1109@users.noreply.github.com>
2023-02-06 02:19:16 +0530

    Major Project 2 containing Partials Layouts and static files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const router=express.Router();
+
 const homeController = require('../controllers/home_controller');
 
-console.log('router loaded');
+const router=express.Router();
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
